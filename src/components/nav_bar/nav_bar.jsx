59ad781a4910ecb9ba38fdb5ac659b620afcc89b
Alin <ady_9697@yahoo.com>
2026-01-11 23:25:41 +0100

    UPDATE: se diseña la vista de telefono para el menu nav bar con un menu hamburgesa con animacion

--- src/components/nav_bar/nav_bar.jsx
+++ src/components/nav_bar/nav_bar.jsx
@@ -2,12 +2,24 @@ import "../../style/nav_bar.css"
 
 const Nav_bar = () => {
 
+    const active_menu = () => {
+    hamburguesa.classList.toggle('active');
+    menu.classList.toggle('active_menu');
+};
+
     return (
 
         <div id="nav_bar">
-            <div className="logo">Logo</div>
-            <div>Description</div>
-            <div id="menu">
+            <div className="nav_info">
+                <div className="logo">Logo</div>
+                <div id="hamburguesa" className="menu_hamburguesa" onClick={active_menu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>Home</div>
+            </div>
+            <div id="menu" className="menu_nav">
                 <div>option_1</div>
                 <div>option_2</div>
                 <div>option_3</div>
